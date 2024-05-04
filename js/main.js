bc1c6e28013b4821395683a9b67861f32ef09d8b
zaki-abed <zaki.abed2541@gmail.com>
2024-05-05 01:00:54 +0300

    Correct loader function behavior

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // Function Loader
-$("window.loader").fadeOut(1000), function() {
-    $("body").css("overflow", "auto");
-    $(this).remove();
-}
+$(window).on('load', function() {
+    $(".overlay").fadeOut(1000, function(){
+        $("body").css("overflow", "auto");
+        $(this).remove();
+    });
+});
