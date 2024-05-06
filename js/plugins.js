1077a04f30c366dec57617f29b80115e8cc824c4
zaki-abed <zaki.abed2541@gmail.com>
2024-05-07 02:51:34 +0300

    Implemented profile page with all tabs using HTML, CSS, and jQuery.

--- js/plugins.js
+++ js/plugins.js
@@ -1,23 +1,120 @@
-// Function Loader
-$(window).on('load', function() {
-    $(".overlay").fadeOut(1000, function(){
-        $("body").css("overflow", "auto");
-        $(this).remove();
+// // Function Loader
+// $(window).on('load', function() {
+//     $(".overlay").fadeOut(1000, function(){
+//         $("body").css("overflow", "auto");
+//         $(this).remove();
+//     });
+// });
+
+// // Index Page :: Make Animation
+
+// setTimeout(function() {
+//     var img = document.getElementById("name");
+//     img.setAttribute("src", "images/khaled-yousef-white.svg");
+// }, 1440); 
+
+// document.getElementById("showPasswordButton").addEventListener("click", function() {
+//     var passwordInput = document.getElementById("passwordInput");
+//     if (passwordInput.type === "password") {
+//         passwordInput.type = "text";
+//     } else {
+//         passwordInput.type = "password";
+//     }
+// });
+
+// Profile.html
+// Accordion Function
+
+$(document).ready(function() {
+
+    // Add click event listener to accordion buttons
+    $('.accordion').click(function() {
+        var row2 = $(this).closest('.box').find('.row-2');
+        if (row2.is(':hidden')) {
+            row2.slideDown().css('display', 'flex');
+        } else {
+            row2.slideUp();
+        }
     });
-});
 
-// Index Page :: Make Animation
+//     // Function Tab: About As
+    // to hide profile page then show about as tab
+    $(".show-edit-about").click(function(){
+        $(".profile").fadeOut(500); 
+        $(".profile-tabs .about-as-tab").css("display", "flex");
+        $(".profile-tabs .about-as-tab").fadeIn(500); 
+    });
 
-setTimeout(function() {
-    var img = document.getElementById("name");
-    img.setAttribute("src", "images/khaled-yousef-white.svg");
-}, 1440); 
+    // to show tab Nudo Changes
+    $(".btn-nudo-changes-about-us").click(function(){
+        $(".nudo-changes-about-us").fadeIn(500); 
+    });
+      
+    // to close tab Nudo Change
+    $(".btn-do-changes-about-us").click(function(){
+        $(".nudo-changes-about-us").fadeOut(500); 
+    });
+    
+    // to close tab Nudo Change
+    $(".nudo-changes-about-us").click(function(){
+        $(".nudo-changes-about-us").fadeOut(500); 
+    });
 
-document.getElementById("showPasswordButton").addEventListener("click", function() {
-    var passwordInput = document.getElementById("passwordInput");
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-    } else {
-        passwordInput.type = "password";
-    }
+    // to hide Tab About As and back to profile page
+    $(".back-to-about").click(function(){
+        $(".profile-tabs .about-as-tab").css("display", "none");
+        $(".about-as-tabs").fadeOut(500);  
+        $(".profile").fadeIn(500); 
+    });
+
+    
+    // Function Tab: Add Experience
+    $(".add-experience").click(function(){
+        $(".profile").fadeOut(500); 
+        $(".profile-tabs .add-experience-tab").fadeIn(500); 
+    });
+    
+    $(".btn-add-experience").click(function(){
+        $(".nudo-changes-add-experience").fadeIn(500); 
+    });
+
+    $(".nudo-changes-add-experience").click(function(){
+        $(".nudo-changes-add-experience").fadeOut(500); 
+    });
+
+    $(".btn-do-changes-add-experience").click(function(){
+        $(".nudo-changes-add-experience").fadeOut(500); 
+    });
+
+    $(".back-to-profile").click(function(){
+        $(".profile-tabs .add-experience-tab").css("display", "none");
+        $(".profile-tabs .add-experience-tab").fadeOut(500);  
+        $(".profile").fadeIn(500); 
+    });
+
+
+    // Function Tab: Edit Experience
+    $(".edit-experience").click(function(){
+        $(".profile").fadeOut(500); 
+        $(".profile-tabs .edit-experience-tab").fadeIn(500); 
+    });
+    
+    $(".btn-edit-experience").click(function(){
+        $(".nudo-changes-edit-experience").fadeIn(500); 
+    });
+
+    $(".nudo-changes-edit-experience").click(function(){
+        $(".nudo-changes-edit-experience").fadeOut(500); 
+    });
+
+    $(".btn-do-changes-edit-experience").click(function(){
+        $(".nudo-changes-edit-experience").fadeOut(500); 
+    });
+
+    $(".back-to-profile").click(function(){
+        $(".profile-tabs .edit-experience-tab").css("display", "none");
+        $(".profile-tabs .edit-experience-tab").fadeOut(500);  
+        $(".profile").fadeIn(500); 
+    });
+    
 });
