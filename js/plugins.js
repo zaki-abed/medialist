80ff4f7aa4b0508ddcce6b2cce7d4e6caa305f21
zaki-abed <zaki.abed2541@gmail.com>
2024-05-07 03:47:16 +0300

    Fix issue in index page

--- js/plugins.js
+++ js/plugins.js
@@ -1,17 +1,12 @@
 //  Index Page :: Make Animation
-    // setTimeout(function() {
-    //     var img = document.getElementById("name");
-    //     img.setAttribute("src", "images/khaled-yousef-white.svg");
-    // }, 1440); 
-
-// document.getElementById("showPasswordButton").addEventListener("click", function() {
-//     var passwordInput = document.getElementById("passwordInput");
-//     if (passwordInput.type === "password") {
-//         passwordInput.type = "text";
-//     } else {
-//         passwordInput.type = "password";
-//     }
-// });
+document.getElementById("showPasswordButton").addEventListener("click", function() {
+    var passwordInput = document.getElementById("passwordInput");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+});
 
 $(document).ready(function() {
 
@@ -108,13 +103,3 @@ $(document).ready(function() {
     });
     
 });
-
-// Function Loader
-$(window).on('load', function() {
-    $(".overlay").fadeOut(1000, function(){
-        $("body").css("overflow", "auto");
-        $(this).remove();
-    });
-});
-
-
